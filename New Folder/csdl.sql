58c87487d5d0c52c599a02bc22432355df3acf81
nBfour1314 <nguyennhathao11c2@gmail.com>
2021-11-11 07:19:23 +0700

    1st3

--- New Folder/csdl.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (Username, Password, Email, Address, Telephone, State) VALUES
-('admin', 'fcea920f7412b5da7be0cf42b8c93759', 'haonngcc19117@fpt.edu.vn', 'Ca Mau City', '0399989631', 1),
-('user', 'fcea920f7412b5da7be0cf42b8c93759', 'user@gmail.com', 'Ca Mau City', '0123456789', 0),
-('user1', 'fcea920f7412b5da7be0cf42b8c93759', 'user1@gmail.com', 'Can Tho', '0987654321', 0);
